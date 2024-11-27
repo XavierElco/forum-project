d54495705651817c80d387daca014e7866262fca
chengmingLi <ccaihong@icloud.com>
2024-11-27 14:50:40 +0800

    Your commit message

---
+++ src/main/java/com/xav/codeproject/CodeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.xav.codeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xav/codeproject/CodeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xav.codeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
